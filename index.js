cf8df874d5147e82ff87147b7a997b4348a35290
cire27 <cireandales18@gmail.com>
2023-02-06 17:06:35 +0800

    Add initial capstone 2 code

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const port = 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+mongoose.set("strictQuery", true);
+mongoose.connect("mongodb+srv://admin123:admin123@cluster0.bfuvdfn.mongodb.net/Casptone-2-Andales?retryWrites=true&w=majority", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+let db = mongoose.connection;
+
+db.on("error", console.log.bind(console, "Connection Error"));
+db.once("open", () => console.log("Connected in the Atlas"));
+
+
+
+
+app.listen(port, () => {
+    console.log(`Connected at port ${port}`);
+})
