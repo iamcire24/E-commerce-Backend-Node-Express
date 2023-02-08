8802376a8a9e61620baedc97ffb42ff56acaefa0
cire27 <cireandales18@gmail.com>
2023-02-08 10:36:12 +0800

    Session 44

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/Product");
 const auth = require("../auth");
-
+const mongoose = require("mongoose");
 
 module.exports.addProduct = async (data) => {
     const {name, code} = data.product
@@ -29,12 +29,62 @@ module.exports.addProduct = async (data) => {
         })
     }
     let message = Promise.resolve("Only ADMIN can add products.")
-    return await message.then((value) => {
-        return {value}
-    })
+    return await message
     
 }
 
 module.exports.getAvailableProducts = () => {
     return Product.find({isActive: true});
 }
+
+module.exports.getProductByName = async (input) => {
+    const productExist = await Product.findOne({name: input.name});
+    if (!productExist){
+        return "Product not found."
+    }
+    return await Product.find({name: input.name}).then(result=> {
+       return result
+    })
+}
+module.exports.getProductByCode = async (input) => {
+    const productExist = await Product.findOne({code: input.code});
+    if (!productExist){
+        return "Product not found."
+    }
+    return await Product.find({code: input.code}).then(result=> {
+       return result
+    })
+}
+
+module.exports.getProductById = async (input) => {
+    if (!mongoose.Types.ObjectId.isValid(input.productId)){
+        return "Product not found"
+    }
+    return await Product.findById(input.productId).then(result=> {
+        return result
+    })
+}
+
+module.exports.updateProductById = async (reqParams, data) => {
+    if (data.isAdmin){
+        let updatedProduct = {
+            name: data.product.name,
+            code: data.product.code,
+            description: data.product.description,
+            price: data.product.price,
+            quantity: data.product.quantity
+        } 
+
+        return await Product.findByIdAndUpdate(reqParams.productId, updatedProduct).then((product, error)=> {
+            if (error){
+                return false
+            } else {
+                return "Succesfully Update"
+            }
+        })
+
+    }
+    let message = Promise.resolve("Only ADMIN can update products.")
+    return await message
+}
+
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -19,6 +19,32 @@ router.get("/availableProducts", (req, res) => {
     productController.getAvailableProducts().then(resultFromController => res.send(resultFromController));
 })
 
+//retrieve single product by name
+router.get("/name", (req, res) =>{
+    productController.getProductByName(req.body).then(resultFromController => res.send(resultFromController));
+})
+
+router.get("/code", (req, res) =>{
+    productController.getProductByCode(req.body).then(resultFromController => res.send(resultFromController));
+})
 
 
+router.get("/:productId", (req,res) => {
+    productController.getProductById(req.params).then(resultFromController => res.send(resultFromController));
+})
+
+router.patch("/updateProduct/:productId", auth.verify, (req, res) => {
+    const data = {
+        product: req.body,
+        isAdmin: auth.decode(req.headers.authorization).isAdmin
+    }
+    productController.updateProductById(req.params, data).then(resultFromController => res.send(resultFromController));
+})
+router.patch("/updateProduct/?productName", auth.verify, (req, res) => {
+    const data = {
+        product: req.body,
+        isAdmin: auth.decode(req.headers.authorization).isAdmin
+    }
+    productController.updateProductByName(req.params, data).then(resultFromController => res.send(resultFromController));
+})
 module.exports = router;
